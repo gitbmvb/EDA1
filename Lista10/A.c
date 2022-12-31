873c0e067ceeaf800ffdcbcece91212a7ff80d84
gitbmvb <bruno.martval@gmail.com>
2022-12-31 17:42:39 -0300

    Exercise A done.

--- Lista10/A.c
+++ Lista10/A.c
@@ -1,35 +1,47 @@
-#include <stdlib.h>
 #include <stdio.h>
-typedef int Item;
-#define lesseq(A, B) (A <= B)
+#include <stdlib.h>
 
-void merge2(Item * v, Item * v1, Item *v2, int l1, int l2, int r1, int r2){
-    Item * aux = malloc(sizeof(Item) * (r1 + r2 - l1 - l2 + 2));
-    int ileft = l1, iright = l2, len = 0;
-    while(ileft <= r1 && iright <= r2){
-        if(lesseq(v1[ileft], v2[iright])) aux[len++] = v1[ileft++];
-        else aux[len++] = v2[iright++];
+int * merge2(int * v1, int * v2, int l1, int l2, int r1, int r2){
+    int * aux = malloc(sizeof(int) * (r2 + r1 - l2 -l1 + 2));
+    int i_v1 = l1, i_v2 = l2, i_aux = 0;
+    while(i_v1 <= r1 && i_v2 <= r2){
+        if(v1[i_v1] <= v2[i_v2]) aux[i_aux++] = v1[i_v1++];
+        else aux[i_aux++] = v2[i_v2++];
     }
-    while(ileft <= r1) aux[len++] = v1[ileft++];
-    while(iright <= r2) aux[len++] = v2[iright++];
-    len = 0;
-    for(int i = 0; i < (r1 + r2 - l1 - l2 + 2); i++) v[i] = aux[len++];
+    while(i_v1 <= r1) aux[i_aux++] = v1[i_v1++];
+    while(i_v2 <= r2) aux[i_aux++] = v2[i_v2++];
+    return aux;
 }
 
-int main(void){
-    int len = 0, n;
-    Item * vetorzao = malloc(sizeof(Item) * len);
-    for(int i = 0; i < 2; i++){
-        scanf("%d", &n);
-        Item * temp = malloc(sizeof(Item) * n);
-        for(int j = 0; j < n; j++){
-            scanf("%d", &temp[j]);
-        }
-        len += n;
-        vetorzao = realloc(vetorzao, sizeof(Item) * len);
-        merge2(vetorzao, vetorzao, temp, 0, 0, len -1, n - 1);
-        free(temp);
+int main(){
+    int lengths[4], final_len = 0;
+    int ** values1 = malloc(sizeof(int*) * 16), ** values2 = malloc(sizeof(int*) * 4), **values3 = malloc(sizeof(int*) * 2);
+    for(int i = 0; i < 4; i++){
+        int len1 = 0, len2 = 0;
+        scanf("%d", &len1);
+        values1[2*i] = malloc(sizeof(int) * len1);
+        for(int j = 0; j < len1; j++) scanf("%d", &values1[2*i][j]);
+        scanf("%d", &len2);
+        values1[2*i + 1] = malloc(sizeof(int) * len2);
+        for(int j = 0; j < len2; j++) scanf("%d", &values1[2*i + 1][j]);
+        values2[i] = malloc(sizeof(int) * (len1 + len2));
+        values2[i] = merge2(values1[2*i], values1[2*i + 1], 0, 0, len1 - 1, len2 - 1);
+        lengths[i] = len1 + len2;
+        free(values1[2*i]);
+        free(values1[2*i + 1]); 
     }
-    for(int i = 0; i < len; i++) printf("%d ", vetorzao[i]);
-    printf("\n");
+    for(int i = 0; i < 2; i++){
+        values3[i] = malloc(sizeof(int) * (lengths[2*i] + lengths[2*i+1]));
+        values3[i] = merge2(values2[2*i], values2[2*i+1], 0, 0, lengths[2*i] - 1, lengths[2*i + 1] - 1);
+        final_len += (lengths[2*i] + lengths[2*i + 1]);
+    }
+    int * v_final = malloc(sizeof(int) * final_len);
+    v_final = merge2(values3[0], values3[1], 0, 0, lengths[0] + lengths[1] - 1, lengths[2] + lengths[3] - 1);
+
+    for(int i = 0; i < final_len; i++){
+        printf("%d ", v_final[i]);
+        if(i + 1 < final_len) printf(" ");
+        else printf("\n");
+    }
+    return 0;
 }
