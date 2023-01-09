301fbe8fc8909168a223b4dd60858cbf75c9b515
gitbmvb <bruno.martval@gmail.com>
2023-01-09 17:28:21 -0300

    Saving changes

--- Lista11/C.c
+++ Lista11/C.c
@@ -1,97 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+struct candidate{int code, votes;};
+typedef struct candidate Item;
+#define exch(A, B) {Item temp = A; B = A; A = temp;}
+#define lesseq(A, B) (B <= A)
 
-struct candidato{
-    int codigo, votos;
-};
-
-typedef struct candidato Item;
-
-int tamanho(int x){
-    if(x/10 == 0) return 1;
-    else return 1 + tamanho(x/10);
-}
-
-int length_of(int x){
-    if(x/10 == 0) return 1;
-    else return 1 + tamanho(x/10);
-}
-
-void make_a_vote(){
-
-}
+int length_of(int x) {return (x/10 == 0) ? 1 : 1 + length_of(x/10);};
 
 int binarysearch(Item * v, int l, int r, int aim){
     if(l > r) return -1;
     int m = (l + r)/2;
-    if(v[m].codigo == aim) return m;
-    else if(aim < v[m].codigo) return binarysearch(v, l, m - 1, aim);
+    if(v[m].code == aim) return m;
+    else if(aim < v[m].code) return binarysearch(v, l, m - 1, aim);
     else return binarysearch(v, m + 1, r, aim);
 }
 
+void make_a_vote(Item ** jobs, int * votes, int * lenghts, int item){
+    if(item < 0) votes[0]++;
+    else{
+        int item_len = length_of(item) - 2;
+        int index_vote = binarysearch(jobs[item_len], 0, lenghts[item_len] - 1, item);
+        if(index_vote < 0){
+            lenghts[item_len]++;
+            jobs[item_len] = realloc(jobs[item_len], sizeof(Item) * lenghts[item_len]);
+            jobs[item_len][lenghts[item_len] - 1].code = item;
+            jobs[item_len][lenghts[item_len] - 1].votes = 1;
+        } else jobs[item_len][index_vote].votes++;
+        votes[1]++;
+    }
+}
 
-int main(void){
-    int lengths[4] = {0, 0, 0, 0}, votes_invalid_valid[2] = {0, 0}, S, F, E;
-    Item ** votes = malloc(sizeof(Item*) * 4);
-    
-    int len_P = 0, len_S = 0, len_F = 0, len_E = 0, item, votos_validos = 0, votos_invalidos = 0;
-    scanf("%d %d %d", &S, &F, &E);
-    Item
-    * presidentes = malloc(sizeof(Item) * len_P),
-    * senadores = malloc(sizeof(Item) * len_S),
-    * federais = malloc(sizeof(Item) * len_F),
-    * estaduais = malloc(sizeof(Item) * len_E);
-    while(scanf("%d", &item) == 1){
-        int tam = tamanho(item);
-        if(tam == 2 && tam > 0){
-            votos_validos++;
-            int teste_codigo = binarysearch(presidentes, 0, len_P - 1, item);
-            if(teste_codigo < 0){
-                len_P++;
-                presidentes = realloc(presidentes, sizeof(Item) * len_P);
-                presidentes[len_P - 1].codigo = item;
-                presidentes[len_P - 1]. votos = 1;
-            } else{
-                presidentes[teste_codigo].votos++;
-            }
-        } else if(tam == 3 && tam > 0){
-            votos_validos++;
-            int teste_codigo = binarysearch(senadores, 0, len_P - 1, item);
-            if(teste_codigo < 0){
-                len_S++;
-                senadores = realloc(senadores, sizeof(Item) * len_S);
-                senadores[len_S - 1].codigo = item;
-                senadores[len_S - 1]. votos = 1;
-            } else{
-                senadores[teste_codigo].votos++;
-            }
-        } else if(tam == 4 && tam > 0){
-            votos_validos++;
-            int teste_codigo = binarysearch(federais, 0, len_P - 1, item);
-            if(teste_codigo < 0){
-                len_F++;
-                federais = realloc(federais, sizeof(Item) * len_F);
-                federais[len_F - 1].codigo = item;
-                federais[len_F - 1]. votos = 1;
-            } else{
-                federais[teste_codigo].votos++;
-            }
-        } else if(tam == 5 && tam > 0){
-            votos_validos++;
-            int teste_codigo = binarysearch(estaduais, 0, len_P - 1, item);
-            if(teste_codigo < 0){
-                len_E++;
-                estaduais = realloc(estaduais, sizeof(Item) * len_E);
-                estaduais[len_E - 1].codigo = item;
-                estaduais[len_E - 1]. votos = 1;
-            } else{
-                estaduais[teste_codigo].votos++;
-            }
-        } else{
-            votos_invalidos++;
+int partition(Item *v, int l, int r){
+    Item c = v[r]; int j = l;
+    for(int k = l; k < r; k++){
+        if(lesseq(v[k].votes, c.votes)){
+            exch(v[j], v[k]);
+            j++;
         }
     }
-    //
+    exch(v[r], v[j]);
+    return j;
+}
 
+void quicksort(Item * v, int l, int r){
+    if(l >= r) return;
+    int j = partition(v, l, r);
+    quicksort(v, l, j - 1);
+    quicksort(v, j + 1, r);
+}
+
+int main(void){
+    int lengths[4] = {0, 0, 0, 0}, votes_IV[2] = {0, 0}, qtdSFE[4] = {1, 0, 0 ,0}, item;
+    Item ** jobs = malloc(sizeof(Item*) * 4);
+    for(int i = 0; i < 4; i++) jobs[i] = malloc(sizeof(Item) * lengths[i]);
+    scanf("%d %d %d", &qtdSFE[1], &qtdSFE[2], &qtdSFE[3]);
+    while(scanf("%d", &item) == 1) make_a_vote(jobs, votes_IV, lengths, item);
+    printf("%d %d\n", votes_IV[1], votes_IV[0]);
+    for(int i = 0; i < 4; i++){
+        quicksort(jobs[i], 0, lengths[i] - 1);
+        for(int j = 0; j < lengths[i]; j++){
+            printf("code = %d, votes = %d\n", jobs[i][j].code, jobs[i][j].votes);
+        }
+        // for(int j = lengths[i] - 1; j >= qtdSFE[i]; j--) (lengths[i] - 1 >= qtdSFE[i]) ? printf("%d ", jobs[i][j].code) : printf("%d\n", jobs[i][j].code);
+        // if(i + 1 < 4) printf("\n");
+    }
 }
